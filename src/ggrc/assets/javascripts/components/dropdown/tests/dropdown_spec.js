9124715324192217affc25146278b60553026ffd
Ivan Kriznar <ivan@simplified.me>
2016-03-31 17:37:51 +0200

    Add tests for dropdown component

---
+++ src/ggrc/assets/javascripts/components/dropdown/tests/dropdown_spec.js
@@ -0,0 +1,11 @@
+/*!
+  Copyright (C) 2016 Google Inc., authors, and contributors <see AUTHORS file>
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+  Created By: ivan@reciprocitylabs.com
+  Maintained By: ivan@reciprocitylabs.com
+*/
+
+describe('GGRC.Components.dropdown', function () {
+  'use strict';
+
+});
