a5dd0d2de985b2ed4b64e6d1c0b02adfb207c170
Ivan Kriznar <ivan@simplified.me>
2016-04-05 14:46:39 +0200

    Improve code style
    Improve code style
    
    Improve code style

--- src/ggrc/models/assessment.py
+++ src/ggrc/models/assessment.py
@@ -21,7 +21,6 @@ from ggrc.models.relationship import Relatable
 from ggrc.models.track_object_state import HasObjectState
 from ggrc.models.track_object_state import track_state_for_class
 from ggrc.services.common import Resource
-from ggrc.models.assessment_template import AssessmentTemplate
 
 
 class Assessment(Assignable, HasObjectState, TestPlanned, CustomAttributable,
@@ -108,8 +107,12 @@ class Assessment(Assignable, HasObjectState, TestPlanned, CustomAttributable,
 
 track_state_for_class(Assessment)
 
+
+# pylint: disable=unused-variable
 @Resource.model_posted_after_commit.connect_via(Assessment)
-def handle_assessment_post(sender, obj=None, src=None, service=None):
+def handle_assessment_post(sender, assessment=None, src=None):
+  """Apply Custom Attribute Definitions and map people as
+  Verifiers and Assessors when generating Assessmet with template"""
   from ggrc.models import all_models
 
   if not src['template']:
@@ -123,57 +126,50 @@ def handle_assessment_post(sender, obj=None, src=None, service=None):
     model = getattr(all_models, model_type, None)
     return db.session.query(model)
 
-  def get_object_owners(obj):
-    return [owner.person for owner in obj.object_owners]
-
   def get_value(obj):
-    type_val = types[obj]
-    return type_val() if callable(type_val) else type_val
+    """Gets person value from string"""
+    types = {
+        'Object Owners': [owner.person for owner in assessment.object_owners],
+        'Audit Lead': audit.contact,
+        'Object Contact': obj.contact,
+        'Primary Contact': obj.contact,
+        'Secondary Contact': obj.secondary_contact,
+        'Primary Assessor': obj.principal_assessor,
+        'Secondary Assessor': obj.secondary_assessor,
+    }
+    return template.default_people[obj]
 
-  def assign_people(people, user_type):
+  def assign_people(people, user_type, relationships):
     people = people if isinstance(people, list) else [people]
     for person in people:
-      rel = filter(lambda rel: rel['source'] == person, people_list)
+      rel = (val for val in relationships if val['source'] == person)
+      rel = next(rel, None)
       if rel:
-        rel = rel[0]
         rel['attrs']['AssigneeType'] += (',' + user_type)
       else:
-        rel = {
-          'source': person,
-          'destination': obj,
-          'context': obj.context,
-          'attrs': {
-            'AssigneeType': user_type,
-          },
-        }
-        people_list.append(rel)
+        relationships.append({
+            'source': person,
+            'destination': assessment,
+            'context': assessment.context,
+            'attrs': {
+                'AssigneeType': user_type,
+            },
+        })
 
-  relationship = getattr(all_models, 'Relationship', None)
   template = get_by_id(src['template'])
-  src_obj = get_by_id(src['object'])
+  obj = get_by_id(src['object'])
   audit = get_by_id(src['audit'])
-  people = template.default_people
   people_list = []
   ca_definitions = get_model('CustomAttributeDefinition').filter_by(
       definition_id=template.id,
       definition_type='assessment_template'
-    ).all()
-  types = {
-    'Object Owners': get_object_owners(src_obj),
-    'Audit Lead': audit.contact,
-    'Object Contact': src_obj.contact,
-    'Primary Contact': src_obj.contact,
-    'Secondary Contact': src_obj.secondary_contact,
-    'Primary Assessor': src_obj.principal_assessor,
-    'Secondary Assessor': src_obj.secondary_assessor,
-  }
+  ).all()
 
-  assign_people(get_value(people['assessors']), 'Assessor')
-  assign_people(get_value(people['verifiers']), 'Verifier')
+  assign_people(get_value('assessors'), 'Assessor', people_list)
+  assign_people(get_value('verifiers'), 'Verifier', people_list)
 
   for person in people_list:
-    rel = relationship(**person)
-    db.session.add(rel)
+    db.session.add(all_models.Relationship(**person))
 
   for definition in ca_definitions:
     db.make_transient(definition)
@@ -181,5 +177,5 @@ def handle_assessment_post(sender, obj=None, src=None, service=None):
     definition.definition_id = obj.id
     definition.definition_type = obj.__tablename__
     definition.title = 'Definition for {}-{}'.format(
-      obj.__tablename__, obj.id)
+        obj.__tablename__, obj.id)
     db.session.add(definition)
