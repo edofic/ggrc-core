2f45b1fc58fcc6805cca96f2660a22992e3da45e
Andraz Bajt <andraz@bajt.me>
2016-10-17 10:41:00 +0200

    bla bla

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,7 @@
 // Karma Configuration
 // Copyright (C) 2016 Google Inc.
 // Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+// meh
 
 module.exports = function (config) {
   var configuration = {
